0ef79d3bc999214b8a7ddea5e7ccbda6ec613cfc
Darran Hayes <2190584+darranhayes@users.noreply.github.com>
2026-07-08 11:04:55 +0100

    It works!

--- Comments.Test/X1Test.cs
+++ Comments.Test/X1Test.cs
@@ -8,17 +8,17 @@ public class X1Test
     [Test]
     public void T1()
     {
-        int a = 7;
-        int b = 12;
+        int lowerBound = 7;
+        int upperBound = 12;
 
         // Expected: sum of squares from 7 to 12
         int expected = 0;
-        for (int i = a; i <= b; i++)
+        for (int i = lowerBound; i <= upperBound; i++)
         {
             expected += i * i;
         }
 
-        int actual = X1.SumOfSquares(a, b);
+        int actual = X1.SumOfSquares(lowerBound, upperBound);
 
         Assert.That(actual, Is.EqualTo(expected));
     }
