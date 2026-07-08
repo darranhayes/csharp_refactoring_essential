20fde6f9c86af76c263a76051a19b7e7b41aba92
Darran Hayes <2190584+darranhayes@users.noreply.github.com>
2026-07-08 12:28:19 +0100

    It works!

---
+++ LegacyCode.Test/ShippingCalculatorTest.cs
@@ -0,0 +1,15 @@
+﻿namespace LegacyCode.Test
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+
+    public class ShippingCalculatorTest
+    {
+        [Test]
+        public void Dummy()
+        {
+            ShippingCalculator calculator = new();
+        }
+    }
+}
